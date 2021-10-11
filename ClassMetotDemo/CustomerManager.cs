bad962bf99e9522b3b88ba575592e8350cbe32e1
Mustafa Edebalı <mustafaedbl@gmail.com>
2021-10-12 01:17:35 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+         public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " kişisi eklendi.");
+        }
+         public void List(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " kişisinin bilgileri listelendi. ");
+        }
+         public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " kişisi silindi. ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+
+            customer1.Id = 111;
+            customer1.Name = "Ali";
+            customer1.Lastname = "Yilmaz";
+            customer1.Jobs = "Doktor";
+
+            Customer customer2 = new Customer();
+
+            customer2.Id = 112;
+            customer2.Name = "Veli";
+            customer2.Lastname = "Yıldız";
+            customer2.Jobs = "Mühendis";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            Console.WriteLine("***************");
+            customerManager.List(customer2);
+            Console.WriteLine("***************");
+            customerManager.Delete(customer1);
+            Console.WriteLine("***************");
+        }
+    }
+}
